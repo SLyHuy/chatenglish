c872c7c4caa431ce1e0b3bb803466dda0f3379cf
Huy Ly <huyly@Huys-MacBook-Pro.local>
2014-09-13 15:53:10 +0700

    add isLiked

--- www/js/controllers/chat-ctrl.js
+++ www/js/controllers/chat-ctrl.js
@@ -81,6 +81,10 @@ ChatApp.controller('ChatCtrl', function($scope, $state, $timeout, $interval, $io
 		//Check Like function
 		if (data.type == 'chat' && data.from == 'system' && data.stranger){
 			$scope.showLike = true;
+
+			if(data.stranger.isLiked){
+				$scope.strangerLiked = true;
+			}
 		}
 
 		if (content){
